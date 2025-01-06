a8ad357565185f605125d64d7fb478867bd78477
ka2er <personseb@wospe.fr>
2025-01-06 23:25:44 +0100

    DEL : remove my network ID

--- src/config.h
+++ src/config.h
@@ -15,7 +15,6 @@ const bool sensorZ2 = true;
 
 // network id & ext.sonde id if they are known
 uint8_t network_id[] = {0xFF, 0xFF, 0xFF, 0xFF};
-uint8_t network_id[] = {0x05, 0xD6, 0x2D, 0x4C};
 
 uint8_t extSon_id = 0xFF;
 #endif // CONFIG_H
