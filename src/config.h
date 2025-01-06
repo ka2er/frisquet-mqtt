4c4b0aa9173713c790234b989cd9d79201343fc6
ka2er <personseb@wospe.fr>
2025-01-06 23:23:56 +0100

    FIX : add default config

--- src/config.h
+++ src/config.h
@@ -2,12 +2,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+const char* ssid = "";  // Mettre votre SSID Wifi
+const char* password = "";  // Mettre votre mot de passe Wifi
 
+const char* mqttServer = ""; // Mettre l'IP du serveur MQTT
 const int mqttPort = 1883;
+const char* mqttUsername = ""; // Mettre le user MQTT
+const char* mqttPassword = ""; // Mettre votre mot de passe MQTT
+uint8_t def_Network_id[] = {0xFF, 0xFF, 0xFF, 0xFF};
 //Future release
 const bool sensorZ2 = true;
 
 // network id & ext.sonde id if they are known
+uint8_t network_id[] = {0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t network_id[] = {0x05, 0xD6, 0x2D, 0x4C};
 
 uint8_t extSon_id = 0xFF;
